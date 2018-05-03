a29cb5766fc32a18ecc5fc7d1f7fc53ccb4a52c8
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2018-05-03 00:10:21 -0700

    Use opt instead of get for JSON parsing with optional strings

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -19,34 +19,34 @@ public class JsonUtils {
     public static Sandwich parseSandwichJson(String json) {
 
 
-        try{
+        try {
 
             // Create a JSONObject from the JSON response string
             JSONObject baseJsonResponse = new JSONObject(json);
 
-            JSONObject names = baseJsonResponse.getJSONObject("name");
-            String mainName = names.getString("mainName");
-            JSONArray akaNameList = names.getJSONArray("alsoKnownAs");
+            JSONObject names = baseJsonResponse.optJSONObject("name");
+            String mainName = names.optString("mainName");
+            JSONArray akaNameList = names.optJSONArray("alsoKnownAs");
             List<String> alsoKnownAs = new ArrayList<>();
 
-            for (int i = 0; i < akaNameList.length() ; i++) {
-                alsoKnownAs.add(akaNameList.getString(i));
+            for (int i = 0; i < akaNameList.length(); i++) {
+                alsoKnownAs.add(akaNameList.optString(i));
             }
 
-            String origin = baseJsonResponse.getString("placeOfOrigin");
-            String description = baseJsonResponse.getString("description");
-            String image = baseJsonResponse.getString("image");
+            String origin = baseJsonResponse.optString("placeOfOrigin");
+            String description = baseJsonResponse.optString("description");
+            String image = baseJsonResponse.optString("image");
 
-            JSONArray ingredientsList = baseJsonResponse.getJSONArray("ingredients");
+            JSONArray ingredientsList = baseJsonResponse.optJSONArray("ingredients");
             List<String> ingredients = new ArrayList<>();
 
-            for (int i = 0; i < ingredientsList.length() ; i++) {
-                ingredients.add(ingredientsList.getString(i));
+            for (int i = 0; i < ingredientsList.length(); i++) {
+                ingredients.add(ingredientsList.optString(i));
             }
 
-            return new Sandwich(mainName,alsoKnownAs,origin,description,image,ingredients);
+            return new Sandwich(mainName, alsoKnownAs, origin, description, image, ingredients);
 
-        }catch (JSONException e) {
+        } catch (JSONException e) {
             // If an error is thrown when executing any of the above statements in the "try" block,
             // catch the exception here, so the app doesn't crash. Print a log message
             // with the message from the exception.
