9df17660f7826c8756de8bc800c4d7a81d9ba531
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2018-05-02 21:14:38 -0700

    set up test details layout and parse name from the json

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -43,7 +44,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +57,20 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+
+        TextView also_known_tv = (TextView) findViewById(R.id.also_known_tv);
+
+        also_known_tv.setText(sandwich.getAlsoKnownAs().toString());
+        TextView origin_tv = (TextView) findViewById(R.id.origin_tv);
+        origin_tv.setText(sandwich.getPlaceOfOrigin().toString());
+
+        TextView ingredients_tv = (TextView) findViewById(R.id.ingredients_tv);
+        ingredients_tv.setText(sandwich.getIngredients().toString());
+
+        TextView description_tv = (TextView) findViewById(R.id.description_tv);
+        description_tv.setText(sandwich.getDescription().toString());
+
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,45 @@
 package com.udacity.sandwichclub.utils;
 
+import android.text.TextUtils;
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        //create a new sandwich
+        Sandwich sandwich = null;
+
+        // If the JSON string is empty or null, then return early.
+        if (TextUtils.isEmpty(json)) {
+            return null;
+        }
+
+        try{
+
+            // Create a JSONObject from the JSON response string
+            JSONObject baseJsonResponse = new JSONObject(json);
+
+            String name = baseJsonResponse.getString("mainName");
+
+
+
+
+        }catch (JSONException e) {
+            // If an error is thrown when executing any of the above statements in the "try" block,
+            // catch the exception here, so the app doesn't crash. Print a log message
+            // with the message from the exception.
+            Log.e("JsonUtils", "Problem parsing the  JSON results", e);
+        }
+
+
+
+
+        return sandwich;
     }
 }
